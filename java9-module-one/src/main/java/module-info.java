720a3d20164ed961189ebfc13369daae93c334e0
Mike Matsumoto <mike.matsumoto@zalando.de>
2020-03-04 15:53:18 +0100

    adding more details about jdeps - java9

--- java9-module-one/src/main/java/module-info.java
+++ java9-module-one/src/main/java/module-info.java
@@ -1,6 +1,14 @@
 module module.one {
     // clients will not have to require again
+    // this is using automatic-module
     requires transitive org.apache.commons.lang3;
+
+    // you can generate the module-info with:
+    //jdeps -verbose:class --generate-module-info . ~/.m2/repository/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar
+    // cat org.apache.commons.lang3/module-info.java
+
+    // after compile you can use jdeps to check your modules dependency
+    // jdeps --module-path ~/.m2/repository/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar -s java9-module-one-1.0-SNAPSHOT.jar
 
     exports java9.module.exported;
 
--- java9-module-two/src/main/java/module-info.java
+++ java9-module-two/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module module.two {
     requires module.one;
 
+    // jdeps for this module
+    // jdeps --module-path ~/.m2/repository/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar:../../java9-module-one/target/java9-module-one-1.0-SNAPSHOT.jar \
+    // -s java9-module-two-1.0-SNAPSHOT.jar
 }
